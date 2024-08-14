64e0b82f3fc0e1729bcd9fa7be5a68b87a5082a5
101 <tmddnjs001133@dsm.hs.kr>
2024-08-15 08:26:20 +0900

    feat: builder 어노테이션 추가

--- src/main/java/com/study/studyjpa/article/GeneralArticle.java
+++ src/main/java/com/study/studyjpa/article/GeneralArticle.java
@@ -3,10 +3,14 @@ package com.study.studyjpa.article;
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.experimental.SuperBuilder;
 
 @Entity
+@SuperBuilder
 @DiscriminatorValue("GENERAL")
 class GeneralArticle extends Article {
     @Column(length = 3)
     private Integer generalLevel;
+
+    protected GeneralArticle() {}
 }
--- src/main/java/com/study/studyjpa/article/PrivateArticle.java
+++ src/main/java/com/study/studyjpa/article/PrivateArticle.java
@@ -3,10 +3,14 @@ package com.study.studyjpa.article;
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.experimental.SuperBuilder;
 
 @Entity
+@SuperBuilder
 @DiscriminatorValue("PRIVATE")
 class PrivateArticle extends Article {
     @Column(length = 3)
     private Integer privateLevel;
+
+    protected PrivateArticle() {}
 }
