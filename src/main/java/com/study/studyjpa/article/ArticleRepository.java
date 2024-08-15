f13f4b31e8ddc79a8795369ef2ec7c42fef3c292
101 <tmddnjs001133@dsm.hs.kr>
2024-08-15 13:40:19 +0900

    feat: @Query를 사용한 메서드 추가

--- src/main/java/com/study/studyjpa/article/ArticleRepository.java
+++ src/main/java/com/study/studyjpa/article/ArticleRepository.java
@@ -1,6 +1,12 @@
 package com.study.studyjpa.article;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.stream.Stream;
 
 interface ArticleRepository extends JpaRepository<Article, Long> {
+    @Query("SELECT p FROM Article p WHERE TYPE(p) = :type")
+    Stream<Article> findArticlesByType(@Param("type") Class<? extends Article> type);
 }
