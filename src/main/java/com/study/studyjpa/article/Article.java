fdf9219cc7650d2d03614ae22cd27da43a14d6af
101 <tmddnjs001133@dsm.hs.kr>
2024-08-15 10:22:25 +0900

    refactor: 사용하지 않는 코드 삭제

--- src/main/java/com/study/studyjpa/article/Article.java
+++ src/main/java/com/study/studyjpa/article/Article.java
@@ -10,7 +10,6 @@ import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.time.Instant;
 import java.time.LocalDateTime;
 
 /**
